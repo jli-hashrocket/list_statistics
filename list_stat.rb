59b8b729f9532c61f533234feb75903dde68d450
macheteshuffle <jeff.li029@gmail.com>
2013-11-16 16:17:40 -0500

    finishing list stats

--- list_stat.rb
+++ list_stat.rb
@@ -1,7 +1,20 @@
 scores = []
-File.open("scores.txt", "r") do |score|
-  scores.each do |score|
-   scores.push score
-   puts score
+File.open("scores.txt", "r") do |score_file|
+  score_file.each do |score|
+    scores.push score
   end
 end
+
+puts "Here are the test scores:"
+scores.each do |item|
+  item.gsub(/[\\n]/,"")
+  item = item.to_i 
+  puts item
+end
+
+lowest = scores.min
+highest = scores.max
+total = scores.inject(0){|sum, num| sum.to_i + num.to_i}
+avg = total / scores.length
+puts "Here is the average score: #{avg.to_f}"
+
